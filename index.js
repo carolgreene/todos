74a6ed51d92d70723fb822616ddf7a396066baa6
carolgreene <carolgreene3@gmail.com>
2020-03-07 10:12:08 -0800

    added button to remove task

--- index.js
+++ index.js
@@ -7,15 +7,29 @@ button.addEventListener("click", function(e) {
   e.preventDefault()
   const newLi = document.createElement("li")
   newLi.innerText = task.value  
+  const doneButton = document.createElement("button")
+  doneButton.innerText = "X"
+  doneButton.id = "doneButton"
   console.log("task.value", task.value)
-  addNewTask(newLi)
+  addNewTask(newLi, doneButton)
   clearTask()
+  removeTask(newLi, doneButton)
 })
 
-function addNewTask(newLi) {
+function addNewTask(newLi, doneButton) {
+  newLi.appendChild(doneButton)
   tasks.appendChild(newLi)
 }
 
 function clearTask() {
   task.value = ''
+}
+
+function removeTask(newLi, doneButton) {
+    console.log(newLi.textContent)
+    doneButton.addEventListener("click", function(e) {
+        tasks.removeChild(newLi)
+    })
+  
+  
 }
